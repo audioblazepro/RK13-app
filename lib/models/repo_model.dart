0fca164d34dc23cc577133e8e02963fb5e85ebb3
Termux-Code <termuxcodepro@correo.com>
2025-05-09 03:34:09 +0000

    ✅ V29 Termux

--- lib/models/repo_model.dart
+++ lib/models/repo_model.dart
@@ -1,12 +1,12 @@
 class RepoModel {
   final String name;
-  final String assetPath;
+  final String scriptFile;
   final String readmeAsset;
   final String githubUrl;
 
   RepoModel({
     required this.name,
-    required this.assetPath,
+    required this.scriptFile,
     required this.readmeAsset,
     required this.githubUrl,
   });
